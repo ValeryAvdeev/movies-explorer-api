b2d4a758cd43bf46ecd2e50902ce3bf5331b82d5
ValeryAvdeev <vkavdeev@gmail.com>
2022-07-30 14:32:28 +0300

    feat: create port env

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { NotFoundError } = require('./error/NotFoundError');
 const errorHandler = require('./middlewares/error');
 
-const { PORT = 3005, dateBase = 'mongodb://localhost:27017/moviesdb' } = process.env;
+const { PORT = 3002, dateBase = 'mongodb://localhost:27017/moviesdb' } = process.env;
 const app = express();
 
 mongoose.connect(dateBase);
